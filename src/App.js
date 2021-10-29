5e7e58e9442adb75bbc0126443c77c1d1d097edd
Nazim Aghayev <nazimagaev277@gmail.com>
2021-10-29 23:49:36 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+function App() {
+  return <h2>accordion project setup</h2>;
+}
+
+export default App;
---
+++ src/components/core/navbar/NavbarItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavbarItems() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavbarItems
